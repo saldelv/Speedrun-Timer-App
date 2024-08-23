fbbf64dfe08283320a3556ea4413439d6fae6b3b
saldev <salvatoredelvecchio77@gmail.com>
2024-08-23 17:01:22 -0400

    Database class update

--- MauiApp1/Components/Helpers/Database.cs
+++ MauiApp1/Components/Helpers/Database.cs
@@ -1,4 +1,5 @@
 ﻿using SQLite;
+using static MauiApp1.Components.Layout.NavMenu;
 using static MauiApp1.Components.Pages.Home;
 namespace MauiApp1.Components.Helpers
 {
@@ -7,29 +8,64 @@ namespace MauiApp1.Components.Helpers
         string filename = "local.db3";
         private SQLiteConnection conn;
 
-        public void Init()
+        public void Init(int type)
         {
             string path = Path.Combine(FileSystem.Current.AppDataDirectory, filename);
             conn = new SQLiteConnection(path);
-            conn.CreateTable<Split>();
+            if (type == 0)
+            {
+                conn.CreateTable<Split>();
+            }
+            else
+            {
+                conn.CreateTable<Run>();
+            }
         }
 
         public List<Split> GetSplits()
         {
-            Init();
+            Init(0);
             return conn.Table<Split>().ToList();
         }
 
-        public void Add(Split split)
+        public void AddSplit(Split split)
         {
-            Init();
+            Init(0);
             conn.Insert(split);
         }
 
-        public void Delete()
+        public List<Run> GetRuns()
         {
-            Init();
-            conn.DeleteAll<Split>();
+            Init(1);
+            List<Run> l = conn.Table<Run>().ToList();
+            if (!l.Any())
+            {
+                Run run = new Run
+                {
+                    Index = 0,
+                    Record = "",
+                    splitIndex = 0,
+                    Name = "Run 1"
+                };
+                l.Add(run);
+                AddRuns(run);
+            }
+            return l;
+        }
+
+        public void AddRuns(Run run)
+        {
+            Init(1);
+            conn.Insert(run);
+        }
+
+        public void Delete(int type)
+        {
+            Init(type);
+            if (type == 0)
+            {
+                conn.DeleteAll<Split>();
+            }
         }
     }
 }
